6f0ff738e5f56dcd06020dbd17fbc778ed86e4b2
Adbhutha Beere <23211a0533@bvrit.ac.in>
2026-03-10 23:00:34 +0530

    Add SSL support for Aiven MySQL

--- backend/config/db.js
+++ backend/config/db.js
@@ -20,6 +20,11 @@ const sequelize = new Sequelize(
             charset: 'utf8mb4',
             collate: 'utf8mb4_general_ci',
         },
+        dialectOptions: {
+            ssl: {
+                rejectUnauthorized: false
+            }
+        }
     }
 );
 
